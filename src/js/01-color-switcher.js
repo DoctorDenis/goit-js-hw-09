ff1becbe91f499d2ba09c8ee2e3186d876bc144f
DoctorDenis <sychovdenis@icloud.com>
2022-06-01 21:59:31 +0300

    Added code to 01-color-switcher.js. Made first task of the homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalTimer = null;
+stopBtn.setAttribute('disabled', '');
 
+startBtn.addEventListener('click', evt => {
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+  intervalTimer = setInterval(() => {
+    document.querySelector('body').style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', evt => {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+  clearInterval(intervalTimer);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
